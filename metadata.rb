0e816e072b8fc6db2029ae0ecfe7b63a7d72d5f3
Kannan Manickam <kannan@rightscale.com>
2013-08-16 21:25:30 -0700

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,8 @@ description      'Installs/Configures minidlna'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.1.0'
 
+%w{ ubuntu debian redhat centos fedora scientific }.each do |os|
+  supports os
+end
+
+recipe 'minidlna', 'Installs and configures minidlna'
